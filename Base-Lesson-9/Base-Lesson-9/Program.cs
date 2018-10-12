f61e7d03d1e036d7f244134059cb93d2884cb4cc
Olga R <rolya85@bk.ru>
2018-10-13 01:26:37 +0300

    L9 Template Added

---
+++ Base-Lesson-9/Base-Lesson-9/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Base_Lesson_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Practice.PracticeExamples.L9_P_EX_1_from_2();
+            //Practice.PracticeExamples.L9_P_EX_2_from_2();
+        }
+    }
+}
